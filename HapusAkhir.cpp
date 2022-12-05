20fb50e27239180c7bd404fa1a51a66996725a4f
diandrakina <119830328+diandrakina@users.noreply.github.com>
2022-12-05 12:42:04 +0700

    Update HapusAkhir.cpp

--- HapusAkhir.cpp
+++ HapusAkhir.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+//NAMANYA ADALAH STACKS, TUJUAN DIA HAPUS DATA TERAKHIR DALAM SEBUAH ARRAY 
 int main(){
 	int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int size = sizeof(arr)/sizeof(arr[0]);
